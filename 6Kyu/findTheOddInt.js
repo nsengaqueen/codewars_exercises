b9f512853ccc3dd78e017b5e0a0e86added86c42
queen <nsengaqueen123@gmail.com>
2024-06-11 11:21:05 +0200

    adding find odd int

---
+++ 6Kyu/findTheOddInt.js
@@ -0,0 +1,15 @@
+function findOdd(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === arr[i]) {
+        count++;
+      }
+    }
+
+    if (count % 2 !== 0) {
+      return arr[i];
+    }
+  }
+}
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
