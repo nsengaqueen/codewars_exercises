49aaece741e9425975fe7b7dbb9d1b79548ec18f
queen <nsengaqueen123@gmail.com>
2024-06-19 11:57:54 +0200

    adding a better way to pig latin

--- 5Kyu/Simplepiglatin.js
+++ 5Kyu/Simplepiglatin.js
@@ -17,3 +17,20 @@ function pigIt(str) {
 
   return result;
 }
+
+//another way
+function pigIt(str) {
+  // Split the input string into an array of words
+  var arrayWord = str.split(" ");
+
+  // Use map() to transform each word
+  return arrayWord
+    .map(function (word) {
+      // Extract the first letter of the word
+      var firstLetter = word.charAt(0);
+
+      // Form the Pig Latin transformation
+      return word.slice(1) + firstLetter + "ay";
+    })
+    .join(" ");
+}
