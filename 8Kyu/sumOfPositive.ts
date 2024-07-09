82e82ed27c0c7c659200c2acd73c6d63b30128d8
queen <nsengaqueen123@gmail.com>
2024-07-09 11:26:31 +0200

    8kyu sum of positive

---
+++ 8Kyu/sumOfPositive.ts
@@ -0,0 +1,10 @@
+export function positiveSum(arr: number[]): number {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
