7844fb716acbdfd0dfd6682e2f76830eeb246790
queen <nsengaqueen123@gmail.com>
2024-07-19 10:31:26 +0200

    adding 5kyu extract Domain name from URL

---
+++ 5Kyu/extractTheDomainNameFromUrl.js
@@ -0,0 +1,7 @@
+function domainName(url) {
+  //your code here
+  url = url.replace("http://", "");
+  url = url.replace("https://", "");
+  url = url.replace("www", "");
+  return url.split(".")[0];
+}
